4bf769b3e656d8cf95e7e8acd9826ff2e7547ae6
Diego Carvalho <diegodmca@outlook.com>
2024-04-15 22:20:07 -0300

    Tab Bar Setup

---
+++ InstagramClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  InstagramClone
+//
+//  Created by Diego Carvalho on 4/15/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
