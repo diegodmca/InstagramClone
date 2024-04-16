86817bdc8084d5b31ce60523ed750a3a82062224
Diego Carvalho <diegodmca@outlook.com>
2024-04-15 22:09:02 -0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Diego Carvalho on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
