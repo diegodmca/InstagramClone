bad873946c463c2f911176118b09af319b2e188e
Diego Carvalho <diegodmca@outlook.com>
2024-04-16 22:14:26 -0300

    Profile View UI Pt.1

--- InstagramClone/Core/Profile/ProfileView.swift
+++ InstagramClone/Core/Profile/ProfileView.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            //Header
+            VStack{
+                //Pics and stats
+                
+                
+                //Name and bio
+                VStack(alignment: .leading, spacing: 4){
+                    Text("John Nada")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    Text("They Live")
+                        .font(.footnote)
+                    
+                }
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                .padding(.horizontal)
+                
+                
+                //Action Button
+                Button{
+                    
+                } label: {
+                    Text("Edit Profile")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 360, height: 32)
+                        .foregroundColor(.black)
+                        .overlay(RoundedRectangle(cornerRadius: 6).stroke(Color.gray, lineWidth: 2))
+                }
+                Divider()
+            }
+            
+            //Post grid View
+            
+            
+            
+            
+        }
     }
 }
 
