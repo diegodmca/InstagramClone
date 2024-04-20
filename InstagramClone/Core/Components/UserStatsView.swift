1b9a579add6f5303bc7cacb614d5d2c0dae2220f
Diego Carvalho <diegodmca@outlook.com>
2024-04-19 22:36:40 -0300

    Profile View

---
+++ InstagramClone/Core/Components/UserStatsView.swift
@@ -0,0 +1,18 @@
+//
+//  UserStatsView.swift
+//  InstagramClone
+//
+//  Created by Diego Carvalho on 4/19/24.
+//
+
+import SwiftUI
+
+struct UserStatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserStatsView()
+}
